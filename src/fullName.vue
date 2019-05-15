26ae5d13d69a2b1f33d35ac8c7634bcfa3239254
Иван <>
2019-05-16 03:10:23 +0800

    init

---
+++ src/fullName.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="firstName">
+                First Name:
+            </label>
+            <input
+                    id="firstName"
+                    type="text"
+                    class="form-control"
+                    :value="firstName"
+                    @input="changeName($event)">
+        </div>
+        <div class="form-group">
+            <label for="lastName">
+                Last Name:
+            </label>
+            <input
+                    id="lastName"
+                    type="text"
+                    class="form-control"
+                    :value="lastName"
+                    @input="changeName($event)">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fullName",
+        props: ['value'],
+        computed: {
+            firstName() {
+                return this.value.split(' ')[0];
+            },
+            lastName() {
+                return this.value.split(' ')[1];
+            }
+        },
+        methods: {
+            changeName(event) {
+                const idField = event.target.id;
+                let fullName = '';
+                if(idField == 'firstName') {
+                    fullName = event.target.value + ' ' + this.lastName;
+                } else {
+                    fullName = this.firstName + ' ' +  event.target.value;
+                }
+                this.$emit('input', fullName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
